cb32d613251230a6751c0c4f70147d55f540db3d
beastsantos <santosugukundasugira@gmail.com>
2024-03-29 11:14:13 +0200

    my first commit

---
+++ form1.js
@@ -0,0 +1,24 @@
+let btn=document.getElementById("button");
+
+btn.addEventListener("click",function() {
+  
+alert("  THIS FORM IS BEAST BASED");
+  
+let q1=prompt("how old are you","");
+let question1=(Number(q1));
+
+if(q1>18){
+ alert("you are allowed to visit this page");
+}else{
+
+alert("you are not allowed to visit this page ")
+
+document.write(" `<br>`this is the end of the fuckn form");
+
+;
+
+
+
+}
+
+})
